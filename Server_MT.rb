a2f69a8522924b47562d7b9563c45b5387f87935
gdabu <geoffdabu@gmail.com>
2015-02-12 18:30:16 -0800

    connections are closed 1-by-1

--- Server_MT.rb
+++ Server_MT.rb
@@ -6,16 +6,32 @@ $i = 0
 server = TCPServer.open(8005)
 printf("Chatserver started on port %d\n", 8005)
 
+mutex = Mutex.new
+
+connections = []
 
 #client disconnects when thread dies
 while 1
-    Thread.fork(server.accept) do |client| 
-		##loop do
+   	Thread.fork(server.accept) do |client| 
+		#puts $i += 1
+		connections.push(client)
+		puts connections.length
+		loop do
+
+
 			line = client.gets
 			client.puts line
 	    	puts line
-	    	sleep
-	    ##end
+			
+			if client.eof?
+				mutex.synchronize do
+					connections.delete(client)
+					puts connections.length
+				end
+				break
+			end
+	    end
+	    
     end #connection dies here
-    puts $i += 1
+    
 end
